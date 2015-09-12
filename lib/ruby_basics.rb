f0e4956dfde415fd0a180ed75f406fbc6500a011
jasongelnick <jason.gelnick@gmail.com>
2015-09-12 14:21:50 -0400

    done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,42 @@
 def division(num1, num2)
- 
+  puts num1, num2
+  return (num1/num2)
+
 end
 
 def assign_variable(value)
+  puts value
+  return value
   
 end
 
-def argue
+def argue (x)
+  puts x
+  return x
   
 end
 
-def greeting
+def greeting (x,y)
+  puts x,y
+  return x,y
 
 end
 
-def return_a_value
+def return_a_value 
+  puts "Nice"
+  return "Nice"
+
 
 end
 
 def last_evaluated_value
+  puts "expert"
+  return "expert"
 
 end
 
-def pizza_party
+def pizza_party (x="cheese")
+  puts x
+  return x
 
 end
